a794abb3023c38dec07c3d69af2a2fcb6e55a16f
ManikMahajan000 <manikmahajan2004@gmail.com>
2025-07-10 02:19:55 +0530

    uptime monitoring - cron

--- backend/server.js
+++ backend/server.js
@@ -19,3 +19,6 @@ app.use('/api/auth', require('./routes/auth'));
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
